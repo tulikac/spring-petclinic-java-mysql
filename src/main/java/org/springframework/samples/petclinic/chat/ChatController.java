fd04cf51815403a5861992f7e8f323fc295cbc9b
denvermb <dbrittain@microsoft.com>
2023-10-05 19:01:17 -0500

    Add chat to pet clinic

---
+++ src/main/java/org/springframework/samples/petclinic/chat/ChatController.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.chat;
+
+import org.springframework.samples.petclinic.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by rajeevkumarsingh on 24/07/17.
+ */
+@Controller
+public class ChatController {
+
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/public")
+	public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		// Add username in web socket session
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@GetMapping("/chat")
+	public String processForm() {
+		return "chat/chat";
+	}
+
+}
