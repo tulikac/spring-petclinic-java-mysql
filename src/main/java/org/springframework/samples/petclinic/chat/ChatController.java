d9dd9fdc91102269cc17f84a64d451c4e78c5b5d
Jeff Martinez <jefmarti@microsoft.com>
2023-10-26 17:05:56 -0700

    add grpc

--- src/main/java/org/springframework/samples/petclinic/chat/ChatController.java
+++ src/main/java/org/springframework/samples/petclinic/chat/ChatController.java
@@ -8,6 +8,9 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
+import io.grpc.*;
+import io.grpc.examples.*;
+
 /**
  * Created by rajeevkumarsingh on 24/07/17.
  */
@@ -17,6 +20,34 @@ public class ChatController {
 	@MessageMapping("/chat.sendMessage")
 	@SendTo("/topic/public")
 	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+
+		// gRPC client //
+		final ManagedChannel channel = ManagedChannelBuilder.forTarget("grpc-java-server.azurewebsites.net").build();
+
+		// String name = "When is microsof ignite?";
+		String name = chatMessage.getContent().toString();
+
+		// It is up to the client to determine whether to block the call
+		// Here we create a blocking stub, but an async stub,
+		// or an async stub with Future are always possible.
+		GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+		GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest.newBuilder()
+				.setName(name).build();
+
+		// Finally, make the call using the stub
+		GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+
+		System.out.println(response.toString());
+		System.out.println("grpc server responded");
+
+		// A Channel should be shutdown before stopping the process.
+		channel.shutdownNow();
+
+		String serverResponse = response.toString();
+
+		// set gRPC response as the chat message
+		chatMessage.setContent(serverResponse);
+
 		return chatMessage;
 	}
 
